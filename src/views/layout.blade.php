ecf0bc7be7673298474d1de1bbb6c720f114ecf1
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2014-07-03 12:05:09 -0300

    Fix html typo pointed by Robson many times

--- src/views/layout.blade.php
+++ src/views/layout.blade.php
@@ -95,7 +95,7 @@
         <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
-                    <h2 class="page-header">{{$title}}</h1>
+                    <h2 class="page-header">{{$title}}</h2>
                 </div>
 
 	            <div class="col-lg-12">
