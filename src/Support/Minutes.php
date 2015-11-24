3bad9965597b2425c45e5b779273744916ccb39e
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-11-24 17:36:28 +0000

    Add getter and setter for dates

--- src/Support/Minutes.php
+++ src/Support/Minutes.php
@@ -2,66 +2,98 @@
 
 namespace PragmaRX\Tracker\Support;
 
-
 use Carbon\Carbon;
 
-class Minutes {
+class Minutes
+{
+    /**
+     * @var
+     */
+    private $minutes;
 
-	private $minutes;
+    /**
+     * @var
+     */
+    private $start;
 
-	private $start;
+    /**
+     * @var
+     */
+    private $end;
 
-	private $end;
+    /**
+     * Minutes constructor.
+     * @param $minutes
+     */
+    public function __construct($minutes) {
+        $this->minutes = $minutes;
 
-	public function __construct($minutes)
-	{
-	    $this->minutes = $minutes;
+        if ($minutes instanceof Minutes) {
+            $this->start = $minutes->getStart();
 
-		if ($minutes instanceof Minutes)
-		{
-			$this->start = $minutes->getStart();
+            $this->end = $minutes->getEnd();
+        }
+        else {
+            $this->calculateStartEnd();
+        }
+    }
 
-			$this->end = $minutes->getEnd();
-		}
-		else
-		{
-			$this->calculateStartEnd();
-		}
-	}
+    /**
+     * Calculate start and end dates
+     */
+    private function calculateStartEnd() {
+        if ($this->minutes == 0) {
+            $this->setToday();
+        }
+        else {
+            $this->start = Carbon::now()->subMinutes($this->minutes);
 
-	public function getStart()
-	{
-		return $this->start;
-	}
+            $this->end = Carbon::now();
+        }
+    }
 
-	public function getEnd()
-	{
-		return $this->end;
-	}
+    /**
+     * @return mixed
+     */
+    public function getEnd() {
+        return $this->end;
+    }
 
-	private function calculateStartEnd()
-	{
-		if($this->minutes == 0)
-		{
-			$this->setToday();
-		}
-		else
-		{
-			$this->start = Carbon::now()->subMinutes($this->minutes);
+    /**
+     * @return mixed
+     */
+    public function getStart() {
+        return $this->start;
+    }
 
-			$this->end = Carbon::now();
-		}
-	}
+    /**
+     * @param $minutes
+     * @return static
+     */
+    public static function make($minutes) {
+        return new static($minutes);
+    }
 
-	private function setToday()
-	{
-		$this->start = Carbon::now()->setTime(0,0,0);
+    /**
+     * @param mixed $end
+     */
+    public function setEnd($end) {
+        $this->end = $end;
+    }
 
-		$this->end = Carbon::now()->setTime(23,59,59);
-	}
+    /**
+     * @param mixed $start
+     */
+    public function setStart($start) {
+        $this->start = $start;
+    }
 
-	public static function make($minutes)
-	{
-		return new static($minutes);
-	}
+    /**
+     * Today
+     */
+    private function setToday() {
+        $this->start = Carbon::now()->setTime(0, 0, 0);
+
+        $this->end = Carbon::now()->setTime(23, 59, 59);
+    }
 }
