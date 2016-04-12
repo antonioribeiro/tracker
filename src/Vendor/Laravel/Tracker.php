eb2da5fee0e9d0637c25bf4057c29258c38d0237
Raines Dave <adadekola@gmail.com>
2016-04-12 21:42:45 +0100

    Update and rename Facade.php to Tracker.php

--- src/Vendor/Laravel/Tracker.php
+++ src/Vendor/Laravel/Tracker.php
@@ -4,7 +4,7 @@ namespace PragmaRX\Tracker\Vendor\Laravel;
 
 use Illuminate\Support\Facades\Facade as IlluminateFacade;
 
-class Facade extends IlluminateFacade {
+class Tracker extends IlluminateFacade {
 
     /**
      * Get the registered name of the component.
