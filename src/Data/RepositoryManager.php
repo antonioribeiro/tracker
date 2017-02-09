9940a47d22163a42b0d9ffd35b03e45ca37b4a6d
Andreas Berger <andiberger@gmx.net>
2017-02-09 14:09:46 +0100

    Laravel 5.4 getUri fix

--- src/Data/RepositoryManager.php
+++ src/Data/RepositoryManager.php
@@ -565,7 +565,7 @@ class RepositoryManager implements RepositoryManagerInterface
             return $name;
         }
 
-        return '/'.$route->current()->getUri();
+        return '/'.$route->current()->uri();
     }
 
     /**
