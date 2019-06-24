93510f6aa8cba79d31e4595007a1e220f9c386f5
Don Cadavona <dcadavona@gmail.com>
2019-06-25 03:38:35 +0800

    Faster fix for Issue #325 on original source.

--- src/Data/Repositories/Session.php
+++ src/Data/Repositories/Session.php
@@ -131,7 +131,11 @@ class Session extends Repository
 
         $wasComplete = true;
 
-        foreach (collect($this->sessionInfo)->except('user_agent')->toArray() as $key => $value) {
+        foreach ($this->sessionInfo as $key => $value) {
+            
+            if ($key === 'user_agent')
+                continue;
+
             if ($sessionData[$key] !== $value) {
                 if (!isset($model)) {
                     $model = $this->find($this->sessionInfo['id']);
